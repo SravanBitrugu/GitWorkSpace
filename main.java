87ba7fecec4220bd440bb7fe67d97b8ea441286c
sravan <sravan@mail.com>
2025-08-29 17:01:38 +0530

    code added 0

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class main{
 public static void main(String [] args){
-System.out.println("adding new ");
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter the n value :: ");
+	int n=sc.nextInt();
+	for(int i=1;i<=n;i++){
+	for (int j=1;j<=i;j++){
+		System.out.print(0);
+	}
+	System.out.println();
+	}
 }
 }
